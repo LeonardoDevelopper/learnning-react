b386984d949eed7cd8e912b80b386ae674ce1407
LeonardoDevelopper <113784384+LeonardoDevelopper@users.noreply.github.com>
2023-07-02 21:08:01 +0100

    adicionei a aula de componentizacao ao repositorio

---
+++ react-components/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+const App = () => {
+  return (
+    <React.Fragment>
+      <h1 style={{color: 'green'}}>
+        OLA MUNDO
+      </h1>
+    </React.Fragment>
+  )
+}
+
+export default App
